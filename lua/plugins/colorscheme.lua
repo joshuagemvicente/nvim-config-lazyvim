bbd35e592d24c30e35d85342579384ac60f087e5
joshuagemvicente6@gmail.com <joshuagemvicente6@gmail.com>
2024-11-18 14:30:11 +0800

    gem configuration from linux mint

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+
+  -- habamax colorscheme configuration
+  -- { "habamax/vim-habamax" },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "habamax",
+  --   }
+  -- }
+
+  {
+    { "bluz71/vim-moonfly-colors" },
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "moonfly",
+      },
+    },
+  },
+}
